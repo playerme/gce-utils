dc5c8bf0f4c8ee12cd04bc066d8a2c563c51e80a
Fabio Brito d'Araujo e Oliveira <psychopenguin@gmail.com>
2018-04-29 23:45:45 +0800

    tests

--- tests/gce_metadata_server.py
+++
@@ -1,6 +0,0 @@
-import gceutils
-
-
-def test_get_info():
-    metadata = gceutils.MetaDataServer()
-    assert metadata.get_info('project/project-id') == "budega"
---
+++ tests/test_gce.py
@@ -0,0 +1,22 @@
+from unittest import mock
+from gce import MetaDataServer
+
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, text, status_code):
+            self.text = text
+            self.status_code = status_code
+    
+    print(args)
+
+    if args[0] == 'http://metadata.google.internal/computeMetadata/v1/project/project-id':
+        return MockResponse('budega', 200)
+    else:
+        return MockResponse(None, 404)
+
+
+@mock.patch('gce.core.requests.get', side_effect=mocked_requests_get)
+def test_get_info(*args):
+    metadata = MetaDataServer()
+    assert metadata.get_info('project/project-id') == "budega"
